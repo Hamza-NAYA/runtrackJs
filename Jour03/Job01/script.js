134c68b7727e8dc3e09059cc917cc883e891682c
Hamza-NAYA <hamza.naya@laplateforme.io>
2024-04-23 23:51:19 +0200

    [Terminer] Jour03

--- Jour03/Job01/script.js
+++ Jour03/Job01/script.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
 
-    $('#showButton').click(function() {
+    $('#show').click(function() {
         $('#citation').removeClass('hidden');
     });
 
-    $('#hideButton').click(function() {
+    $('#hide').click(function() {
         $('#citation').addClass('hidden');
     });
 });
